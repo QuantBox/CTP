d058452f7016446efad45704bb262b3344c5a05c
伍侃 <wu-kan@163.com>
2014-06-30 16:55:14 +0800

    修改OrderIndex的类型

--- CSharp-CTP/src/QuantBox.Libray/OrderIndexType.cs
+++ CSharp-CTP/src/QuantBox.Libray/OrderIndexType.cs
@@ -5,9 +5,13 @@ using System.Text;
 
 namespace QuantBox.Libray
 {
+    /// <summary>
+    /// 默认使用FIX 5.0中的协议字段来填充
+    /// </summary>
     public class OrderIndexType
     {
-        public const int OpenClose = 0;
-        public const int HedgeFlag = 1;
+        public const int Account = 1;
+        public const int PositionEffect = 77;
+        public const int HedgeFlag = 255; // 投机与保值，这个不知道对应的是哪一个，先这样用着
     }
 }
