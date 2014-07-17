29622668668f3db79231a9e6f289273f2a712fad
伍侃 <wu-kan@163.com>
2014-07-17 11:10:09 +0800

    修改部分FIX Tag

---
+++ CSharp-CTP/src/QuantBox.Library/OrderMsgType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuantBox.Library
+{
+    public class OrderMsgType
+    {
+        public const char NewOrderSingle = 'D';
+        public const char QuoteRequest = 'R';
+        public const char Quote = 'S';
+        public const char QuoteCancel = 'Z';
+
+        // 表示此Order完全忽视
+        public const char Ignore = '\0';
+    }
+}
--- CSharp-CTP/src/QuantBox.Library/OrderTagType.cs
+++ CSharp-CTP/src/QuantBox.Library/OrderTagType.cs
@@ -8,10 +8,16 @@ namespace QuantBox.Library
     /// <summary>
     /// 默认使用FIX 5.0中的协议字段来填充
     /// </summary>
-    public class OrderIndexType
+    public class OrderTagType
     {
+        public const int Zero = 0;
+
         public const int Account = 1;
+        public const int MsgType = 35;
         public const int PositionEffect = 77;
-        public const int HedgeFlag = 255; // 投机与保值，这个不知道对应的是哪一个，先这样用着
+
+        public const int HedgeFlag = 5000; // 投机与保值，这个不知道对应的是哪一个，先这样用着
+
+        public const int Order = 9999; // 特殊的用于记录关联的Order
     }
 }
