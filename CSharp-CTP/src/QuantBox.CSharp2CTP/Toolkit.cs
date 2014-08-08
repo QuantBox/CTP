377050c0cc02142ff6432e5fa0592ee0a40f2ddd
伍侃 <wu-kan@163.com>
2014-08-08 09:50:37 +0800

    加入设置Dll目录功能

---
+++ CSharp-CTP/src/QuantBox.CSharp2CTP/Toolkit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace QuantBox.CSharp2CTP
+{
+    public class Toolkit
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetDllDirectory(string lpPathName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int GetDllDirectory(int bufSize, StringBuilder buf);
+
+
+        public static string GetEntryAssemblyPath()
+        {
+            //string _CodeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            string _CodeBase = System.Reflection.Assembly.GetEntryAssembly().CodeBase;
+
+            _CodeBase = _CodeBase.Substring(8, _CodeBase.LastIndexOf("/") - 8 + 1); // 8是 "file://" 的长度
+
+            return _CodeBase.Replace("/", @"\");
+            //return System.AppDomain.CurrentDomain.BaseDirectory;
+        }
+    }
+}
